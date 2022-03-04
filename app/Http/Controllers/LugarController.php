9b22d470246007c94678d771f2ff3e2034653604
marcdcc <100007197.joan23@fje.edu>
2022-03-04 16:31:21 +0100

    commit

---
+++ app/Http/Controllers/LugarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lugar;
+use Illuminate\Http\Request;
+
+class LugarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lugar  $lugar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lugar $lugar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lugar  $lugar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lugar $lugar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lugar  $lugar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lugar $lugar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lugar  $lugar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lugar $lugar)
+    {
+        //
+    }
+}
